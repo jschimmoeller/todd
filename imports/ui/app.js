87408ac8b60100fb075480f0fe5e43f89aae778e
jschimmoeller@schimmoeller.net <jschimmoeller@schimmoeller.net>
2016-09-04 21:49:44 -0400

    header stuff

--- imports/ui/app.js
+++ imports/ui/app.js
@@ -4,13 +4,42 @@ import Header from './header';
 
 // App component - represents the whole app
 export default class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {isSettingsOpen: false};
+  }
   render(){
-    return (
-      <div style={{display: "flex", flexDirection: "column", minHeight: "100vh"}}>
-        <Header />
-        <div style={{ margin: "auto", flex: "5 100%"}}>App is here </div>
-        <Footer />
-      </div>
-    );
+    const settingsComponent = <img src='./settings.png' width="25px" height="25px" onClick={()=>{
+      console.log('services');
+      this.setState({isSettingsOpen: !this.state.isSettingsOpen});
+    }}/>;
+
+    if (this.state.isSettingsOpen){
+      const backComponent = <img src='./arrowleft.png' width="25px" height="25px" onClick={()=>{
+        console.log('back');
+        this.setState({isSettingsOpen: !this.state.isSettingsOpen});
+      }}/>;
+      return (
+        <div style={{display: "flex", flexDirection: "column", minHeight: "100vh"}}>
+          <Header title="TODD Settings" componentLeft={backComponent}  componentRight={settingsComponent} />
+          <div style={{ margin: "auto", flex: "5 100%"}}>
+            Services
+          </div>
+          <Footer />
+        </div>
+      );
+    } else {
+
+
+      return (
+        <div style={{display: "flex", flexDirection: "column", minHeight: "100vh"}}>
+          <Header title="TODD" componentRight={settingsComponent}/>
+          <div style={{ margin: "auto", flex: "5 100%"}}>App is here </div>
+          <Footer />
+        </div>
+      );    }
+
+
   }
 }
--- imports/ui/header.js
+++ imports/ui/header.js
@@ -1,10 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 
 // App component - represents the whole app
 export default class Header extends Component {
   render(){
     return (
-      <div style={{ height: "50px", margin: "auto" }}>Header</div>
+      <div style={{ display: 'flex', direction: "row",  height: "50px", margin: "auto" }}>
+      <div style={{margin: "auto", left: "10px", top: "8px", position: "absolute"}}>
+        {this.props.componentLeft}
+      </div>
+        <div style={{margin: "auto"}}>{this.props.title}</div>
+        <div style={{margin: "auto", right: "10px", top: "8px", position: "absolute"}}>
+          {this.props.componentRight}
+        </div>
+      </div>
     );
   }
 }
+
+Header.propTypes = {
+  // This component gets the task to display through a React prop.
+  // We can use propTypes to indicate it is required
+  title: PropTypes.string.isRequired,
+  componentLeft: PropTypes.object,
+  componentRight: PropTypes.object,
+};
