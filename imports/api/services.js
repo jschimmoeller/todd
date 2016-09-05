738b673891bdf15ea4c4dddd0f2713c58ee16043
jschimmoeller@schimmoeller.net <jschimmoeller@schimmoeller.net>
2016-09-04 23:02:45 -0400

    services

--- imports/api/services.js
+++ imports/api/services.js
@@ -1,3 +1,36 @@
+
+import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
+import { check } from 'meteor/check';
 
 export const Services = new Mongo.Collection('services');
+
+
+Meteor.methods({
+ 'services.insert'(title, featureCode) {
+   console.log()
+   check(title, String);
+   check(featureCode, String);
+
+
+   Services.insert({
+     title,
+     featureCode,
+     createdAt: new Date(),
+     modifiedAt: new Date()
+   });
+ },
+ 'services.remove'(serviceId) {
+   check(serviceId, String);
+
+   Services.remove(serviceId);
+ },
+ 'services.update'(serviceId, title, featureCode ) {
+   check(serviceId, String);
+   check(title, String);
+   check(featureCode, String);
+
+
+   Services.update(serviceId, { $set: { title, featureCode, modifiedAt: new Date() } });
+ },
+});
--- imports/ui/app.js
+++ imports/ui/app.js
@@ -1,14 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { createContainer } from 'meteor/react-meteor-data';
+import { Meteor } from 'meteor/meteor';
+
+import { Services } from '../api/services.js';
+
 import Footer from './footer';
 import Header from './header';
 
 // App component - represents the whole app
-export default class App extends Component {
+class App extends Component {
   constructor(props){
     super(props);
 
     this.state = {isSettingsOpen: false};
   }
+
+  renderServices(){
+    return this.props.services.map((s)=>{
+      console.log('>>>>', s);
+      return (
+        <li key={s._id} style={{listStyle: "none"}}>
+          {s.title} - {s.featureCode}
+          <button onClick={()=>{
+            console.log('dddd');
+            Meteor.call('services.remove', s._id);
+          }}>Delete</button>
+        </li>
+      );
+    });
+  }
+
   render(){
     const settingsComponent = <img src='./settings.png' width="25px" height="25px" onClick={()=>{
       console.log('services');
@@ -24,7 +45,34 @@ export default class App extends Component {
         <div style={{display: "flex", flexDirection: "column", minHeight: "100vh"}}>
           <Header title="TODD Settings" componentLeft={backComponent}  componentRight={settingsComponent} />
           <div style={{ margin: "auto", flex: "5 100%"}}>
-            Services
+              <input
+                type="text"
+                ref="titleInput"
+                placeholder="Title for Service"
+              />
+              <input
+                type="text"
+                ref="featureCodeInput"
+                placeholder="Feature Code"
+              />
+              <button onClick={()=>{
+                //TODO do i have data
+                const title = this.refs.titleInput.value.trim();
+                const featureCode = this.refs.featureCodeInput.value.trim();
+
+
+                Meteor.call('services.insert', title, featureCode);
+
+                // Clear form
+                this.refs.titleInput.value = '';
+                this.refs.featureCodeInput.value = '';
+
+                console.log('submitting new form')
+              }}>Save</button>
+
+            <ul>
+              {this.renderServices()}
+            </ul>
           </div>
           <Footer />
         </div>
@@ -43,3 +91,12 @@ export default class App extends Component {
 
   }
 }
+App.propTypes = {
+  services: PropTypes.array.isRequired,
+};
+
+export default createContainer(() => {
+  return {
+    services: Services.find({}).fetch(),
+  };
+}, App);
