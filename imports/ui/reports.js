b1694fb9bee380e0e6cd75bf4963d668c970b104
jschimmoeller@schimmoeller.net <jschimmoeller@schimmoeller.net>
2016-09-11 15:15:13 -0400

    reports  - added date

--- imports/ui/reports.js
+++ imports/ui/reports.js
@@ -1,19 +1,27 @@
 import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
-import Calendar from 'react-input-calendar'
+import Calendar from 'react-input-calendar';
+import moment from 'moment';
 
 
 // App component - represents the whole app
 class Reports extends Component {
   constructor(props){
     super(props);
+    this.state={reportDate: moment().format('MM-DD-YYYY') }
   }
 
   render(){
     return (
       <div>
         <div>reports</div>
-        <Calendar format='DD/MM/YYYY' />
+        <Calendar format='MM-DD-YYYY' date={this.state.reportDate} onChange={(d)=>{
+          console.log('Date Changed:', d);
+          this.setState({ ...this.state, reportDate: d });
+        }}/>
+        <button onClick={()=>{
+          console.log('clicking retrieve', this.state.reportDate);
+        }} >Retrieve</button>
       </div>
     )
   }
