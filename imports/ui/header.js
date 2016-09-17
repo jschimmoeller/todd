3988369cc66ab7af217de0509705835da4052a02
knicholsc <knichols@star2star.com>
2016-09-17 15:49:40 -0400

    added styles to header component

--- imports/ui/header.js
+++ imports/ui/header.js
@@ -6,14 +6,67 @@ import { CalendarIconSVG } from './svgs';
 export default class Header extends Component {
   render(){
     return (
-      <div style={{ display: 'flex', direction: "row",  height: "50px" }}>
-        <div style={{margin: "auto", left: "10px", top: "8px", position: "absolute"}}>
+      <div
+          style={{
+            backgroundColor: '#239d82',
+            color: 'white',
+            display: 'flex',
+            direction: "row",
+            height: "70px" }}
+      >
+        <div
+            style={{
+              margin: "auto",
+              left: "10px",
+              top: "8px",
+              position: "absolute"}}
+        >
           {this.props.componentLeft}
         </div>
-        <div style={{margin: "auto"}}>
-          <Link to="/reports"><CalendarIconSVG />{this.props.title}</Link>
+        <div
+            style={{
+              margin: "auto" }}
+        >
+          <Link
+              to="/reports"
+              style={{
+                display: 'flex',
+                flexDirection:'column',
+                alignItems: 'center',
+                color: '#fff',
+                textDecoration: 'none',
+                fontFamily: 'avenir',
+                fontSize: '18px',
+                fontStretch: 'normal',
+                fontWeight: '900'}}
+          >
+            <div
+                style={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  width: '24px',
+                  height: '24px',
+                  borderRadius: '50px',
+                  backgroundColor: '#25a586'}}
+            >
+              <CalendarIconSVG
+                  svgStyle={{
+                    width: '16px',
+                    height: '16px',
+                    fill: '#ffffff'}}
+              />
+            </div>
+            {this.props.title}
+          </Link>
         </div>
-        <div style={{margin: "auto", right: "10px", top: "8px", position: "absolute"}}>
+        <div
+            style={{
+              margin: "auto",
+              right: "10px",
+              top: "8px",
+              position: "absolute"}}
+        >
           {this.props.componentRight}
         </div>
       </div>
@@ -24,7 +77,7 @@ export default class Header extends Component {
 Header.propTypes = {
   // This component gets the task to display through a React prop.
   // We can use propTypes to indicate it is required
-  title: PropTypes.string.isRequired,
   componentLeft: PropTypes.object,
   componentRight: PropTypes.object,
+  title: PropTypes.string.isRequired
 };
