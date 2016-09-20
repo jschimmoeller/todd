4c8d11673d4ae3cd7e002a877b95370f9eb8bd03
knicholsc <knichols@star2star.com>
2016-09-19 20:25:27 -0400

    updated buttons in settings

--- imports/ui/serviceItem.js
+++ imports/ui/serviceItem.js
@@ -13,11 +13,58 @@ class ServiceItem extends Component {
     }
 
     return (
-      <li style={{listStyle: "none"}}>
-        <div>
+      <li
+        style={{
+          alignItems: 'center',
+          display: 'flex',
+          justifyContent: 'space-between',
+          listStyle: "none",
+          padding: '5px'
+        }}
+      >
+        <div
+          style={{
+            display: 'flex',
+            flex: '1',
+            justifyContent: 'space-between'
+          }}
+        >
         {this.props.item.title}
-        <button onClick={()=>{this.props.cbEdit(this.props.item._id)}}>Edit</button>
-        <button onClick={()=>{this.props.cbDelete(this.props.item._id)}}>Delete</button>
+        <div>
+        <button
+            onClick={()=>{
+              this.props.cbEdit(this.props.item._id);
+            }}
+            style={{
+              backgroundColor: '#fff',
+              border: 'none',
+              borderRadius: '50px',
+              color: '#25a586',
+              fontSize: '14px',
+              height: '30px',
+              marginRight: '10px',
+              width: '70px'
+            }}
+        >
+            Edit
+        </button>
+        <button
+            onClick={()=>{
+              this.props.cbDelete(this.props.item._id);
+            }}
+            style={{
+              backgroundColor: '#239d82',
+              border: 'solid 0.5px #fff',
+              borderRadius: '50px',
+              color: '#fff',
+              fontSize: '14px',
+              height: '30px',
+              width: '70px'
+            }}
+        >
+            Delete
+        </button>
+        </div>
         </div>
         {editComponent}
       </li>
